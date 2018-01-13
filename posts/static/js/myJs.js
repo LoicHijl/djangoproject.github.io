cb82e63c968a141031c4acc1d97074adfbd63f65
LoicHijl <34099350+LoicHijl@users.noreply.github.com>
2018-01-13 16:55:45 +0100

    Added slide-section

--- posts/static/js/myJs.js
+++ posts/static/js/myJs.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+    /* Sliding animation when clicking on a link */
+    $('.slide-section').click(function(e) {
+    var linkHref = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top
+    });
+
+    e.preventDefault();
+  });
     
+    /* */
     $("[data-toggle=tooltip]").tooltip();
 });
